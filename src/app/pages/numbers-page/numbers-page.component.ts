f259903a0a4de8193585ee37bbdf56907cd58698
Omar Diaz <odiaz.dev@gmail.com>
2025-05-22 09:28:49 -0600

    decimal percent currency - pipes

--- src/app/pages/numbers-page/numbers-page.component.ts
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
   standalone: true,
-  imports: [],
+  imports: [DecimalPipe,PercentPipe,CurrencyPipe],
   templateUrl: './numbers-page.component.html',
 })
-export default class NumbersPageComponent { }
+export default class NumbersPageComponent {
+  totalSell=signal(2_433_232.5567);
+  percent=signal(0.4856);
+}
