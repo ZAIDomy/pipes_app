604f517a84b6b40e907232a17a9dd8eb868947c3
Omar Diaz <odiaz.dev@gmail.com>
2025-05-26 15:01:18 -0600

    toggleCase Pipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { toggleCasePipe } from '../../pipes/toggle-case-pipe';
 
 @Component({
   selector: 'app-custom-page',
   standalone: true,
-  imports: [],
+  imports: [toggleCasePipe],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+  name = signal('alex diaz');
+
+  upperCase = signal(true);
+
+  toggleSignal(){
+    this.upperCase.update(value=>!value);
+  }
+
+
+}
