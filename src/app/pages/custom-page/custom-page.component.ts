55985b069d440fe3ca3eacc265ecf0fc910a961d
Omar Diaz <odiaz.dev@gmail.com>
2025-05-27 11:35:44 -0600

    heroTextColor Creator - pipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -4,11 +4,13 @@ import { heroes } from '../../data/heroes.data';
 import { canFlyPipe } from '../../pipes/canFly.pipe';
 import { heroColorPipe } from '../../pipes/heroColor.pipe';
 import { creatorPipe } from '../../pipes/creator.pipe';
+import { heroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-custom-page',
   standalone: true,
-  imports: [toggleCasePipe, canFlyPipe, heroColorPipe,creatorPipe],
+  imports: [toggleCasePipe, canFlyPipe, heroColorPipe, creatorPipe, heroTextColorPipe, TitleCasePipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
